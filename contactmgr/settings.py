be1417f4ec6ccf9ea49582a42f72fecf62dd829c
Pulkit <shahpulkit@gmail.com>
2026-05-04 20:20:00 +0530

    new fixes

--- contactmgr/settings.py
+++ contactmgr/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'contacts',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'contactmgr.urls'
